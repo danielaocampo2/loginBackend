73d687b7ae55c4c9c40f0ba8a380c08a73aa4b35
Daniela_Ocampo <danielaocampoo@gmail.com>
2020-10-26 12:49:30 -0500

    new port

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ require('./database');
 app.use(cors()); // agrega cabecera a la petecion para poder ser pasados a esta peticion
 app.use(express.json()); // convierte los datos que recibe el js a un formato que pueda modificar
 app.use('/api', require('./routes/index'));
-app.listen(3000);
-console.log('server on port', 3000);
+app.listen(process.env.PORT || 3000); // 3000
+//console.log('server on port', 3000);
